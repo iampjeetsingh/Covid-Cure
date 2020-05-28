31cdb305ba0638d902c0b9dfc3fe3ea3c66e493e
Vishal Pal <pal1vishal0001@gmail.com>
2020-05-28 09:23:23 +0530

    Modified splash screen

--- app/src/main/java/com/example/hp_awareness_app/SplashScreen.java
+++ app/src/main/java/com/example/hp_awareness_app/SplashScreen.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,6 +15,29 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        final ImageView main = (ImageView) findViewById(R.id.final_img);
+        final FrameLayout frameLayout = (FrameLayout) findViewById(R.id.frame);
+        main.animate().alpha(1).setDuration(3500);
+        new CountDownTimer(3500, 1000) { // 5000 = 5 sec
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                main.animate().translationYBy(-450).setDuration(800);
+            }
+        }.start();
+        new CountDownTimer(4500, 1000) { // 5000 = 5 sec
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                frameLayout.animate().alpha(1).setDuration(1000);
+            }
+        }.start();
+
+
     }
 
     @Override
@@ -21,6 +47,6 @@ public class SplashScreen extends AppCompatActivity {
             Intent intent = new Intent(SplashScreen.this,PhoneActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
-        },4500);
+        },8000);
     }
 }
