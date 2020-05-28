145e101ab71b88359836d858af0690094cbb9eb4
arjun anand <arjunluam0732@gmail.com>
2020-05-28 09:39:07 +0530

    Splash screen final

--- app/src/main/java/com/example/hp_awareness_app/SplashScreen.java
+++ app/src/main/java/com/example/hp_awareness_app/SplashScreen.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -15,6 +17,13 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        //changing statusbar color
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.black));
+        }
         final ImageView main = (ImageView) findViewById(R.id.final_img);
         final FrameLayout frameLayout = (FrameLayout) findViewById(R.id.frame);
         main.animate().alpha(1).setDuration(3500);
@@ -47,6 +56,6 @@ public class SplashScreen extends AppCompatActivity {
             Intent intent = new Intent(SplashScreen.this,PhoneActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
-        },8000);
+        },9500);
     }
 }
