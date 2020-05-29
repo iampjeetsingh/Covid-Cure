2d9e4ddad9f1bcf8b559c1f0d768e54a79b9d7f3
arjun anand <arjunluam0732@gmail.com>
2020-05-29 21:37:13 +0530

    Dialogues added

--- app/src/main/java/com/example/hp_awareness_app/NewActivity.java
+++ app/src/main/java/com/example/hp_awareness_app/NewActivity.java
@@ -1,6 +1,8 @@
 package com.example.hp_awareness_app;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,13 @@ public class NewActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //changing statusbar color
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.clr_grey_text_transparent));
+        }
+
     }
 }
