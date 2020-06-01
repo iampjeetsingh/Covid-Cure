f4c4c561b3bcc5fd9f10543532060dc908e2e9b0
arjun anand <arjunluam0732@gmail.com>
2020-06-01 18:42:55 +0530

    Bug fixing -4

--- app/src/main/java/com/example/hp_awareness_app/NewActivity.java
+++ app/src/main/java/com/example/hp_awareness_app/NewActivity.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -61,6 +63,12 @@ public class NewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.black));
+        }
         toolbar = findViewById(R.id.mytoolbar);
         drawerLayout = findViewById(R.id.drawer);
         type = getIntent().getStringExtra("type");
